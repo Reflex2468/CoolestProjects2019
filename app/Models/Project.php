eab92b89e41be548330a6c81099e5e1720d5e49d
Reflex2468 <xavier.coppens@digitalmove.be>
2019-04-23 20:47:48 +0200

    Added function getURL() and getProjectId() to the model: Project

--- app/Models/Project.php
+++ app/Models/Project.php
@@ -6,5 +6,12 @@ use Illuminate\Database\Eloquent\Model;
 
 class Project extends Model
 {
-    //
+    public function getProjectId() {
+        $project_id = preg_replace('/^.*\/project\/([\d]+[^_]).*$/', '$1', $this->path_to_thumbnail);
+        return $project_id;
+    }
+
+    public function getURL() {
+        return "https://scratch.mit.edu/projects/{$this->getProjectId()}/";
+    }
 }
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -10,9 +10,16 @@
 
                 <div class="uk-child-width-1-4@xl uk-child-width-1-4@l uk-child-width-1-3@m uk-child-width-1-1@s uk-grid uk-grid-match" uk-grid>
                     @foreach(\App\Models\Project::all() as $project)
-                        <div class="uk-card uk-card-default uk-padding-small" style="background: no-repeat center/cover url( {{$project->path_to_thumbnail}} )">
+                        <div class="uk-card uk-card-default uk-padding-small" style="box-sizing: border-box;">
+                            <img src="{{$project->path_to_thumbnail}}" alt="thumbnail">
                             <h3 class="uk-card-title">{{$project->title}}</h3>
-                            <p>{{$project->description}}</p>
+                            <p>
+                                @if($project->description === "lorem ipsum")
+                                    klik <a class="text-info" href="{{$project->getURL()}}">hier</a> om het project te bekijken.
+                                @else
+                                    {{$project->description}}
+                                @endif
+                            </p>
                         </div>
                     @endforeach
                 </div>
