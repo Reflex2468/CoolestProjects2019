40096ce6e65ed05959018a64765c90a5596841c1
Reflex2468 <xavier.coppens@digitalmove.be>
2019-04-24 18:42:31 +0200

    Updated the UI of the feed

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,27 +4,20 @@
 
 @section('content')
     <div class="container mt-3">
-        <div>
-
-            <div class="uk-position-relative uk-visible-toggle uk-light" tabindex="-1" uk-slideshow="animation: pull; autoplay: true; autoplay-interval: 6000">
-
-                <div class="uk-child-width-1-4@xl uk-child-width-1-4@l uk-child-width-1-3@m uk-child-width-1-1@s uk-grid uk-grid-match" uk-grid>
-                    @foreach(\App\Models\Project::all() as $project)
-                        <div class="uk-card uk-card-default uk-padding-small" style="box-sizing: border-box;">
-                            <img src="{{$project->path_to_thumbnail}}" alt="thumbnail">
-                            <h3 class="uk-card-title">{{$project->title}}</h3>
-                            <p>
-                                @if($project->description === "lorem ipsum")
-                                    klik <a class="text-info" href="{{$project->getURL()}}">hier</a> om het project te bekijken.
-                                @else
-                                    {{$project->description}}
-                                @endif
-                            </p>
-                        </div>
-                    @endforeach
+        <div class="row">
+            @foreach(\App\Models\Project::all() as $project)
+                <div class="col-md-3">
+                    <img style="width: 100%;" src="{{$project->path_to_thumbnail}}" alt="thumbnail">
+                    <h3>{{$project->title}}</h3>
+                    <p>
+                        @if($project->description === "lorem ipsum")
+                            klik <a class="text-info" href="{{$project->getURL()}}">hier</a> om het project te bekijken.
+                        @else
+                            {{$project->description}}
+                        @endif
+                    </p>
                 </div>
-            </div>
-
+            @endforeach
         </div>
     </div>
 @endsection
