fe01fb58a2a28e01af903839797a1b62a3fa376e
sharethewisdom <de.roy.bart@gmail.com>
2019-10-17 00:48:33 +0200

    footer mod. and css grid overwrites for supporting browsers.

--- resources/views/includes/footer.blade.php
+++ resources/views/includes/footer.blade.php
@@ -6,25 +6,11 @@
 
     footer {
         overflow-x: hidden;
-        position: relative;
-        float: left;
         width: 100%;
-        min-height: 40vh;
         background: var(--footer-background-color);
         color: #999;
-        padding: 32px 12px;
+        padding: 20px;
         box-sizing: border-box;
-    }
-    footer .logo {
-        height: 100%;
-    }
-    footer .logo img {
-        width:100%;
-        position: relative;
-        top: 50%;
-        transform: translateY(-50%);
-        margin: 0 auto;
-        min-width: 128px;
     }
     footer h3 {
         color: #fff;
@@ -46,7 +32,7 @@
         background: white;
     }
     footer ul {
-        margin-top: 24px;
+        margin-top: 18px;
         list-style: none;
         padding: 0;
     }
@@ -78,25 +64,16 @@
         width: 80%;
     }
     footer .copyright {
-        margin-top: 32px;
+        margin-top: 20px;
         font-family: Arial, Helvetica, sans-serif;
-        padding: 24px 12px;
+        padding: 12px;
         border-top: 1px solid rgba(255, 255, 255, .2);
         text-align: center;
     }
 
-
-
     @media only screen and (max-width: 1161px) {
         footer .col-md {
             padding: 0 1.5em;
-            width: 50% !important;
-            flex-basis: unset;
-        }
-
-        footer .logo img {
-            width: 50%;
-            min-width: 128px;
         }
 
         footer .pc-only {
@@ -108,27 +85,22 @@
     }
 
 </style>
-<footer class="uk-margin-large-top">
+<footer>
     <div class="row justify-content-center">
-        <div class="col-xs-12 col-sm-12 col-md-8 col-lg-8 footer-content">
+        <div class="col-xs-12 col-sm-12 col-md-6 col-lg-6 footer-content">
             <div class="row">
-                <div class="col-md-4">
-                    <div class="logo" style="width: 50%; margin: 0 auto;">
-                        <img src="{{asset('images/coderdojo_logo_white.png')}}" alt="Coderdojo">
-                    </div>
-                </div>
-                <div class="col-md-4">
-                    <h3>Informatie</h3>
+                <div class="col-md-6">
+                    <h3>Links</h3>
                     <ul>
                         <li class="nowrap"><a href="https://www.facebook.com/groups/635078973250800/" target="_blank">Facebook-pagina</a></li>
                         <li class="nowrap"><a href="https://github.com/Reflex2468/CoolestProjects2019" target="_blank">GitHub-pagina</a></li>
                         <li class="nowrap"><a href="https://www.coolestprojects.be/" target="_blank">Coolest Projects</a></li>
                     </ul>
                 </div>
-                <div class="col-md-4">
-                    <h3>Made by</h3>
+                <div class="col-md-6">
+                    <h3>Gemaakt door</h3>
                     <ul>
-                        <li>Deze site is gemaakt door Coderdojo Eeklo <br>Neal&nbsp;Joos, Xavier&nbsp;Coppens & Bart&nbsp;De&nbsp;Roy</span></li>
+                        <li>Neal&nbsp;Joos, Xavier&nbsp;Coppens & Bart&nbsp;De&nbsp;Roy</span></li>
                     </ul>
                 </div>
             </div>
@@ -138,13 +110,3 @@
         </div>
     </div>
 </footer>
-
-<script>
-
-    window.onload = function() {
-        if ($(window).height() > ($('nav').height() + $('.container').height() + $('footer').height() ) ) {
-            $('footer').addClass('uk-position-bottom');
-        }
-    }
-
-</script>
